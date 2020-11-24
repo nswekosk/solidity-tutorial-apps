7bbce608e6c8f2cebb10b18346c0c1a005b611db
Nick Sweko <nicholas_swekosky@yahoo.com>
2020-11-24 10:50:50 -0600

    master Updated ability to change value on simple storage app

---
+++ 3_simple_storage/frontend/public/bundle.js
@@ -0,0 +1,64 @@
+const simpleStorageABI = [{
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "x",
+        "type": "uint256"
+      }
+    ],
+    "name": "set",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+},
+{
+    "constant": true,
+    "inputs": [],
+    "name": "get",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}];
+const simpleStorageAddress = '0x71cd8598195abb5FC7AD56824D82c90242c5aFAE';
+const web3 = new Web3('http://localhost:9545');
+const simpleStorage = new web3.eth.Contract(simpleStorageABI, simpleStorageAddress);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const $setData = document.getElementById('setData');
+    const $data = document.getElementById('data');
+    let accounts = [];
+
+    web3.eth.getAccounts()
+    .then(_accounts => {
+        accounts = _accounts;
+    });
+
+    const getData = () => {
+        simpleStorage.methods
+        .get()
+        .call()
+        .then(result => {
+            $data.innerHTML = result;
+        });
+    }
+
+    getData();
+
+    $setData.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const data = e.target.elements[0].value;
+        simpleStorage.methods
+        .set(data)
+        .send({from: accounts[0]})
+        .then(getData);
+    });
+});
